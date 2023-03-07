6c3a1feb9a53b21c38d11bb7f04a04933e16463f
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-03-07 14:22:08 +0100

    Added solutiona nd productModle/Repository

---
+++ Lab2Webb/ProductDataAccess/Models/ProductModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProductDataAccess.Models
+{
+	public class ProductModel
+	{
+		[BsonId] 
+		public ObjectId ProductId { get; set; }
+
+		[BsonElement]
+		public string ProductName { get; set; }
+
+		[BsonElement] 
+		public decimal Price { get; set; }
+
+		[BsonElement]
+		public string ProductDescription { get; set; }
+		[BsonElement]
+		public string ProductType { get; set; }
+
+		[BsonElement]
+		public string Status { get; set; }
+	}
+}
---
+++ Lab2Webb/ProductDataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using ProductDataAccess.Models;
+
+namespace ProductDataAccess.Repositories
+{
+	public class ProductRepository
+	{
+		private readonly IMongoCollection<ProductModel> _products;
+
+		public ProductRepository()
+		{
+			var host = "localhost";
+			var databaseName = "NetChat";
+			var port = 27017;
+			var connectionString = $"mongodb://{host}:{port}";
+			var client = new MongoClient(connectionString);
+			var database = client.GetDatabase(databaseName);
+		}
+	}
+}
