13ab88d6d5d4f3e204d8d197ca3f0c3c53a94132
alexgoold <102036021+alexgoold@users.noreply.github.com>
2023-03-16 16:35:33 +0100

    Order Repo started

--- Lab2Webb/ProductDataAccess/Repositories/OrderRepository.cs
+++ Lab2Webb/ProductDataAccess/Repositories/OrderRepository.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using ProductDataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lab2Webb.Shared.DTOs;
+using MongoDB.Bson;
 
 namespace ProductDataAccess.Repositories
 {
-	internal class OrderRepository
+	public class OrderRepository
 	{
+		private readonly IMongoCollection<OrderModel> _orders;
+
+		public OrderRepository()
+		{
+			var databaseName = "Labb2Webb";
+			var port = 27017;
+			var settings = MongoClientSettings.FromConnectionString("mongodb+srv://mongo:mongo123@cluster0.t4yoico.mongodb.net/test");
+			var client = new MongoClient(settings);
+			var database = client.GetDatabase(databaseName);
+			_orders =
+				database.GetCollection<OrderModel>("Orders",
+					new MongoCollectionSettings() { AssignIdOnInsert = true });
+		}
+
+		public async void CreateOrder(OrderModel order)
+		{
+			await _orders.InsertOneAsync(order);
+		}
+
+		}
+
 	}
 }
--- Lab2Webb/Shared/DTOs/OrderDTO.cs
+++ Lab2Webb/Shared/DTOs/OrderDTO.cs
@@ -1,9 +1,8 @@
-﻿
-namespace Lab2Webb.Shared.DTOs;
+﻿namespace Lab2Webb.Shared.DTOs;
 
 public class OrderDTO
 {
-	public CustomerDTO Customer { get; set; }
+	public  CustomerDTO Customer { get; set; }
 
 	public List<ProductDTO> Products { get; set; }
 
